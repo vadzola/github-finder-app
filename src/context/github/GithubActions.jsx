0c618da635e56dad70f51f061a4e1cab4f8ae4a3
vadzola <wadzola@yahoo.com.hk>
2022-03-07 22:51:07 +0100

    prvi i zadnji commit3

--- src/context/github/GithubActions.jsx
+++ src/context/github/GithubActions.jsx
@@ -1,31 +1,28 @@
-import axios from "axios";
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+import axios from 'axios'
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+//const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 const github = axios.create({
-    baseURL: GITHUB_URL,
-    headers: { Authorization: `token ${GITHUB_TOKEN}`}
+  baseURL: GITHUB_URL,
+ // headers: { Authorization: `token ${GITHUB_TOKEN}` },
 })
 
- // Search users
- export const searchUsers = async (text) => {
-      const params = new URLSearchParams({
-      q: text,
-    });
+// Get search results
+export const searchUsers = async (text) => {
+  const params = new URLSearchParams({
+    q: text,
+  })
 
-     const response = await github.get(`/search/users?${params}`)
-     return response.data.items
-  };
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
+}
 
-   // Get single user
-   export const getUserAndRepos = async (login) => {
-     const [user, repos] = await Promise.all([
-         github.get(`/users/${login}`),
-         github.get(`/users/${login}/repos`)
-     ])
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ])
 
-     return { user: user.data, repos: repos.data}
-
-   }
-
-   
+  return { user: user.data, repos: repos.data }
+}
