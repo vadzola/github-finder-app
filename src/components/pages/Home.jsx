fee908e3b0ef31439d9dedf096a7f627e1f2d897
vadzola <wadzola@yahoo.com.hk>
2022-03-07 16:48:27 +0100

    prvi i zadnji commit2

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserSearch from "../users/UserSearch"
+import UserResults from "../users/UserResults"
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+        <UserResults />
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
+}
